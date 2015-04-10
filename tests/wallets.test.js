278c208e2381e4394fc2c8e5c974b70c9ca3aba0
justinvdm <justinvdm.0.1.0@gmail.com>
2015-04-10 13:31:56 +0200

    minor cleanup for wallet test requires

--- tests/wallets.test.js
+++ tests/wallets.test.js
@@ -5,7 +5,7 @@ var assert = require('assert'),
     walletSdk = require('stellar-wallet-js-sdk'),
     app = require('../src/app'),
     fixtures = require('./fixtures'),
-    Promise = require('bluebird').Promise;
+    Promise = require('bluebird');
 
 
 describe("/wallets/", function() {
