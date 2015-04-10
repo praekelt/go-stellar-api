a28342c3f1c794bd21b5de58b959e1c20ab09fdc
justinvdm <justinvdm.0.1.0@gmail.com>
2015-04-10 13:27:43 +0200

    use fixtureSet test helper for wallet fixtures

--- tests/fixtures/wallets.js
+++ tests/fixtures/wallets.js
@@ -1,65 +1,112 @@
 var extend = require('extend'),
-    walletSdk = require('stellar-wallet-js-sdk');
+    walletSdk = require('stellar-wallet-js-sdk'),
+    fixtureSet = require('./fixture-set');
 
 
-module.exports = function(username) {
-  return {
-    'spam@ham.org': fixture({
-      server: 'http://fakewalletserver.org/v2',
-      username: 'spam@ham.org',
-      password: 't00r',
-      keyPair: {
-        address: 'gajc65uAsK4AgQM771SqkntWyVQL48GBjK',
-        secret: 'sfSU4rF2UuKTCQG5kTukEqNsuUgUTgf2UuC2gsEokxtypwCeJbZ',
-        secretKey: 'JcLMbIfE9revV7Mj6uVoJapXemG5itPOcu1e1RpvUEC3Vp0MuWQM+AGaeRqF50cSiNZJxX4lX+nEaAVhGW88xg==',
-        publicKey: 't1adDLlkDPgBmnkahedHEojWScV+JV/pxGgFYRlvPMY='
-      },
-      mainData: '',
-      totpEnabled: false,
-      kdfParams: {
-        algorithm: 'scrypt',
-        bits: 256,
-        n: 2,
-        r: 2,
-        p: 1
-      },
-      masterKey: [
-        -1830265364,
-        -1060254570,
-        1582592107,
-        -1153518659,
-        1094928358,
-        -1750137082,
-        1844095556,
-        1487140878
-      ],
-      walletId: [
-        -1342793472,
-        -1175319241,
-        -2095488028,
-        -584959001,
-        1196271854,
-        -1396960380,
-        430012030,
-        -563619323
-      ],
-      walletKey: [
-        -908878483,
-        566593407,
-        -1172243769,
-        1318745934,
-        977827846,
-        -439033250,
-        1277965531,
-        -1050275274
-      ]
-    })
-  }[username];
-};
+module.exports = fixtureSet()
+  .add('spam@ham.org', fixture({
+    server: 'http://fakewalletserver.org/v2',
+    username: 'spam@ham.org',
+    password: 't00r',
+    keyPair: {
+      address: 'gajc65uAsK4AgQM771SqkntWyVQL48GBjK',
+      secret: 'sfSU4rF2UuKTCQG5kTukEqNsuUgUTgf2UuC2gsEokxtypwCeJbZ',
+      secretKey: 'JcLMbIfE9revV7Mj6uVoJapXemG5itPOcu1e1RpvUEC3Vp0MuWQM+AGaeRqF50cSiNZJxX4lX+nEaAVhGW88xg==',
+      publicKey: 't1adDLlkDPgBmnkahedHEojWScV+JV/pxGgFYRlvPMY='
+    },
+    mainData: '',
+    totpEnabled: false,
+    kdfParams: {
+      algorithm: 'scrypt',
+      bits: 256,
+      n: 2,
+      r: 2,
+      p: 1
+    },
+    masterKey: [
+      -1830265364,
+      -1060254570,
+      1582592107,
+      -1153518659,
+      1094928358,
+      -1750137082,
+      1844095556,
+      1487140878
+    ],
+    walletId: [
+      -1342793472,
+      -1175319241,
+      -2095488028,
+      -584959001,
+      1196271854,
+      -1396960380,
+      430012030,
+      -563619323
+    ],
+    walletKey: [
+      -908878483,
+      566593407,
+      -1172243769,
+      1318745934,
+      977827846,
+      -439033250,
+      1277965531,
+      -1050275274
+    ]
+  }))
+  .add('eggs@ham.org', fixture({
+    server: 'http://fakewalletserver.org/v2',
+    username: 'eggs@ham.org',
+    password: 'r00t',
+    kdfParams: {
+      algorithm: 'scrypt',
+      bits: 256,
+      n: 2,
+      r: 2,
+      p: 1
+    },
+    masterKey: [
+      -679916462,
+      -1550398217,
+      -1551266150,
+      -1929235214,
+      -216986543,
+      342694780,
+      1925262505,
+      -1820042274
+    ],
+    walletId: [
+      -632544014,
+      -2131742615,
+      -430146105,
+      -1176756687,
+      -1446227894,
+      -304491366,
+      1692485075,
+      -837484427
+    ],
+    walletKey: [
+      1934014606,
+      -1459061741,
+      -1927411001,
+      1654647685,
+      -595060025,
+      -801144034,
+      -539896453,
+      -1936240292
+    ],
+    mainData: '',
+    keyPair: {
+      address: 'gLk2FP9PFw1TFAt7kyAVuJpr4vLapiVRSJ',
+      secret: 's3VJj1XUuJKgDAc95jyJYN5mSvZZk7gbnkmD2PNPQz5x3DdQZ1Y',
+      secretKey: 'sIShOWVGPcXhF/yXvz5zHOUKFKe8XxhoMqmQviVC7RRnibz7CcHChsUfS/mKcGPElbz0MZy02QN8s8b94nboaw==',
+      publicKey: 'Z4m8+wnBwobFH0v5inBjxJW89DGctNkDfLPG/eJ26Gs='
+    }
+  }));
 
 
 function fixture(data) {
-  return extend(data, {
+  return extend({
     wallet: walletSdk.createFromData({
       server: data.server,
       username: data.username,
@@ -71,5 +118,5 @@ function fixture(data) {
       lockVersion: data.lockVersion,
       totpEnabled: data.totpEnabled
     })
-  });
+  }, data);
 }
